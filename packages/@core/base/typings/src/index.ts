0d8094f93b94c7bcf63c3cc345cea1e178aec313
caishancha-zs <lcshuxiaoluo@163.com>
2025-09-30 16:11:43 +0800

    feat: 新增全局核心类型 @react-admin-core/typings 模块

---
+++ packages/@core/base/typings/src/index.ts
@@ -0,0 +1,6 @@
+export type * from './app';
+export type * from './basic';
+export type * from './helper';
+export type * from './menu';
+// export type * from './tabs';
+// export type * from './react-router';
