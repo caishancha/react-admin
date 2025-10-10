263d12ac2172d55721aae2c7abf382062cf3fe66
caishancha-zs <lcshuxiaoluo@163.com>
2025-10-10 14:33:37 +0800

    fix: 修改

--- apps/web-antd/src/App.tsx
+++ apps/web-antd/src/App.tsx
@@ -1,15 +1,8 @@
 import { RouterProvider } from 'react-router';
 import router from '#/router';
-import { Button } from '@react-admin-core/shadcn-ui';
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-      <div className="text-red-500 rounded-lg z-100 bg-primary">1223</div>
-      <Button variant="destructive">Button</Button>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
