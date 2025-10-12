98c5015b7e931c0705355e981ecdd36e1c16852f
xiaoyuan-zs <xiaoyuanZs@outlook.com>
2025-10-12 21:52:59 +0800

    feat: 内部包增加utils工具

---
+++ packages/@core/base/shared/src/constants/react.ts
@@ -0,0 +1,22 @@
+/**
+ * @zh_CN GITHUB 仓库地址
+ */
+export const REACT_GITHUB_URL = 'https://github.com/vbenjs/vue-vben-admin';
+
+/**
+ * @zh_CN 文档地址
+ */
+export const REACT_DOC_URL = 'https://doc.vben.pro';
+
+/**
+ * @zh_CN React Logo
+ */
+export const REACT_LOGO_URL =
+  'https://unpkg.com/@vbenjs/static-source@0.1.7/source/logo-v1.webp';
+
+/**
+ * @zh_CN React Admin 首页地址
+ */
+export const REACT_PREVIEW_URL = 'https://www.vben.pro';
+
+export const REACT_ANT_PREVIEW_URL = 'https://ant.vben.pro';
