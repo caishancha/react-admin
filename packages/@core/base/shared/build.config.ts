5b876a2e9c57690c3636ea39d406141128804507
caishancha-zs <lcshuxiaoluo@163.com>
2025-10-14 10:22:54 +0800

    feat(@react-admin-core/shared): 完善内部包shared模块

--- packages/@core/base/shared/build.config.ts
+++ packages/@core/base/shared/build.config.ts
@@ -3,5 +3,12 @@ import { defineBuildConfig } from 'unbuild';
 export default defineBuildConfig({
   clean: true,
   declaration: true,
-  entries: ['src/utils/index', 'src/cache/index'],
+  entries: [
+    'src/store',
+    'src/utils/index',
+    'src/cache/index',
+    'src/constants/index',
+    'src/color/index',
+    'src/global-state',
+  ],
 });
